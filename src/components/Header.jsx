a0daf417ed757274d426b745f85c24ff4c4c909d
fedibbm <benbrahimfedi4@gmail.com>
2024-10-13 23:15:53 +0100

    version 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-[#6B0B0C] pt-8 pb-6 rounded-b-[30px] mb-16'>
+        <h1 className='text-[2.5rem] text-white text-center'>Sultan Aalep</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import FoodCategory from '../components/FoodCategory'
+import data from "../../data.json";
+import Header from '../components/Header'
+
+const HomePage = () => {
+    useEffect(()=>{
+        console.log(data);
+    },[])
+  return (
+    <div>
+        <Header />
+        <div className='grid grid-cols-2 w-full place-items-center gap-y-4'>
+            {data.map(category=><FoodCategory title={category.title} picSrc={category.img} />)}       
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
