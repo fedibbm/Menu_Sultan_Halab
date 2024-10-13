5f686d3fd6bb1984f9a1c383ab80661c4499bc48
fedibbm <benbrahimfedi4@gmail.com>
2024-10-13 23:22:25 +0100

    added phone perspective

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <HomePage />
+    <div className='bg-[#dec58e] md:w-[425px] md:h-[914px] md:absolute md:overflow-scroll md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2' >
+      <HomePage />
+    </div>
   ) 
 }
 
