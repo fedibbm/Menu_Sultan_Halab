dd3f6598e6ef6787af054dd106db2dcff2231863
fedibbm <benbrahimfedi4@gmail.com>
2024-10-13 23:37:33 +0100

    tweaked width in md view

--- src/components/FoodCategory.jsx
+++ src/components/FoodCategory.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FoodCategory = ({title, picSrc}) => {
   return (
     <div className='flex flex-col'>
-        <img className="h-28 w-[45vw] rounded-md border border-gray-500 " src={picSrc} />
+        <img className="h-28 w-[45vw] md:w-[191.25px] rounded-md border border-gray-500" src={picSrc} />
         <p className='text-center text-lg font-semibold '>{title}</p>
     </div>
   )
