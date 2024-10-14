8f73be023112fd62faa5b4cd213dc8507d737f61
fedibbm <benbrahimfedi4@gmail.com>
2024-10-14 18:17:42 +0100

    second option for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full bg-[#6B0B0C] pt-8 pb-6 rounded-b-[30px] mb-16'>
-        <h1 className='text-[2.5rem] text-white text-center'>Sultan Aalep</h1>
+    <div className='w-full rounded-b-[30px] py-8 pt-16 flex justify-center'>
+        <h1 className='text-[2rem] bg-[#feffef] max-w-fit px-3 font-bold underline text-center text-red-900' >Sultan Aalep</h1>
     </div>
   )
 }
